12f8229a96a3118feede688b441878d31f387b74
Magitoneu <mtoneupanicot@gmail.com>
2018-01-10 12:50:13 +0100

    MLP, kNN

---
+++ knnCAT.R
@@ -0,0 +1,33 @@
+library(MASS)
+library(class)
+library(knncat)
+load('ws.rdata')
+
+
+# args = commandArgs(TRUE)
+# if (NA %in% args[1]) {
+#   stop("NA")
+# }
+# drug = args[1]
+
+drug = 'Cannabis'
+df = data.factor[, c(2:13, grep(drug, colnames(data.factor)))]
+df[, drug] = factor(df[, drug])
+
+N <- nrow(df)
+learn <- sample(1:N, round(0.67*N))
+nlearn <- length(learn)
+ntest <- N - nlearn
+
+learn.inputs <- df[learn, 1:12]
+learn.classes <- df[learn, drug]
+test.inputs <- df[-learn, 1:12]
+test.classes <- df[-learn, drug]
+
+ks <- c(1:sqrt(nrow(learn.inputs)))
+
+myknn <- knncat(df[learn,], df[-learn,], k = ks, xvals = 10, classcol = 13)
+synpred <- predict(myknn, df[learn,], df[-learn,], train.classcol = 13, newdata.classcol = 3)
+
+tab <- table(synpred, df[-learn, drug])
+1 - sum(tab[row(tab)==col(tab)])/sum(tab)
