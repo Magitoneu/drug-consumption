f89a44bcc60ea65cb2acef355b4d74db8d4e1934
daniconde <daniabc2@gmail.com>
2018-01-14 14:17:53 +0100

    Add plots.R

---
+++ plots.R
@@ -0,0 +1,72 @@
+#!/usr/bin/Rscript
+library(ggplot2)
+load("ws.rdata")
+
+plotsdir = "plots-dir"
+dir.create(plotsdir)
+
+# ==================================================================================
+# ==================================================================================
+# http://www.cookbook-r.com/Graphs/Multiple_graphs_on_one_page_(ggplot2)/
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+  library(grid)
+  plots <- c(list(...), plotlist)
+  numPlots = length(plots)
+  if (is.null(layout)) {
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                    ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+ if (numPlots==1) {
+    print(plots[[1]])
+  } else {
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+    for (i in 1:numPlots) {
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row, layout.pos.col = matchidx$col))
+    }
+  }
+}
+# ==================================================================================
+# ==================================================================================
+a = lapply( data.factor[,14:31], function(x) rbind(
+    CL0 = sum(x == "CL0") ,
+    CL1 = sum(x == "CL1") ,
+    CL2 = sum(x == "CL2") ,
+    CL3 = sum(x == "CL3") ,
+    CL4 = sum(x == "CL4") ,
+    CL5 = sum(x == "CL5") ,
+    CL6 = sum(x == "CL6")
+))
+write.csv(data.frame(a), paste(plotsdir, "/all.csv", sep=""))
+# ==================================================================================
+# ==================================================================================
+df = data.factor[,14:31]
+pl = lapply(seq_along(df), function(x) {
+    a = data.frame(df[,x])
+    ggplot(a, aes_string(x=colnames(a)[1])) + geom_bar() + labs(x=names(df)[x], y=NULL) + ylim(0, 1700)
+})
+png(filename=paste(plotsdir, "/all.png", sep=""), width=210, height=297, units="mm", res=200)
+multiplot(plotlist=pl, cols=2)
+dev.off()
+# ==================================================================================
+# ==================================================================================
+df = data.factor[,14:23]
+pl = lapply(seq_along(df), function(x) {
+    a = data.frame(df[,x])
+    ggplot(a, aes_string(x=colnames(a)[1])) + geom_bar() + labs(x=names(df)[x], y=NULL) + ylim(0, 1700)
+})
+png(filename=paste(plotsdir, "/all-sub-00.png", sep=""), width=210, height=250, units="mm", res=200)
+multiplot(plotlist=pl, cols=2)
+dev.off()
+# ==================================================================================
+# ==================================================================================
+df = data.factor[,24:31]
+pl = lapply(seq_along(df), function(x) {
+    a = data.frame(df[,x])
+    ggplot(a, aes_string(x=colnames(a)[1])) + geom_bar() + labs(x=names(df)[x], y=NULL) + ylim(0, 1700)
+})
+png(filename=paste(plotsdir, "/all-sub-01.png", sep=""), width=210, height=200, units="mm", res=200)
+multiplot(plotlist=pl, cols=2)
+dev.off()
+
