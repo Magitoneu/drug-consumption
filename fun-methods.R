15f0b628bec68d1aa4738df2b3f2cfd93cabb72d
daniconde <daniabc2@gmail.com>
2018-01-16 23:27:30 +0100

    Generalize number of classes for random forest weighted

--- fun-methods.R
+++ fun-methods.R
@@ -47,23 +47,19 @@ funmeth.randomForest.weighted = function (data, drug) {
     df = data[, c(2:13, grep(drug, colnames(data)))]
     N = nrow(df)
     learn = sample(1:N, round(0.9*N))
-    n0 = nrow(df[intersect(learn, which(df[,drug] == "CL0")),])
-    n1 = nrow(df[intersect(learn, which(df[,drug] == "CL1")),])
-    n2 = nrow(df[intersect(learn, which(df[,drug] == "CL2")),])
-    n3 = nrow(df[intersect(learn, which(df[,drug] == "CL3")),])
-    n4 = nrow(df[intersect(learn, which(df[,drug] == "CL4")),])
-    n5 = nrow(df[intersect(learn, which(df[,drug] == "CL5")),])
-    n6 = nrow(df[intersect(learn, which(df[,drug] == "CL6")),])
-    nm = max(c(n0, n1, n2, n3, n4, n5, n6))
-    d0 = df[rep_len(row.names(df[intersect(learn, which(df[,drug] == "CL0")),]), length.out=nm),]
-    d1 = df[rep_len(row.names(df[intersect(learn, which(df[,drug] == "CL1")),]), length.out=nm),]
-    d2 = df[rep_len(row.names(df[intersect(learn, which(df[,drug] == "CL2")),]), length.out=nm),]
-    d3 = df[rep_len(row.names(df[intersect(learn, which(df[,drug] == "CL3")),]), length.out=nm),]
-    d4 = df[rep_len(row.names(df[intersect(learn, which(df[,drug] == "CL4")),]), length.out=nm),]
-    d5 = df[rep_len(row.names(df[intersect(learn, which(df[,drug] == "CL5")),]), length.out=nm),]
-    d6 = df[rep_len(row.names(df[intersect(learn, which(df[,drug] == "CL6")),]), length.out=nm),]
-    dr = rbind(d0, d1, d2, d3, d4, d5, d6)
-    model = randomForest(as.formula(paste(drug, " ~ .")), data = dr, ntree=150, proximity=FALSE, sampsize=c(CL0=nm, CL1=nm, CL2=nm, CL3=nm, CL4=nm, CL5=nm, CL6=nm), strata=dr[,drug])
+    lvls = levels(df[,drug])
+    nm = 0
+    for (lvl in lvls) {
+        print(lvl)
+        print(nrow(df[intersect(learn, which(df[,drug] == lvl)),]))
+        nm = max(nm, nrow(df[intersect(learn, which(df[,drug] == lvl)),]))
+    }
+    dr = data.frame()
+    for (lvl in lvls) {
+        dlvl = df[rep_len(row.names(df[intersect(learn, which(df[,drug] == lvl)),]), length.out=nm),]
+        dr = rbind(dr, dlvl)
+    }
+    model = randomForest(as.formula(paste(drug, " ~ .")), data = dr, ntree=150, proximity=FALSE)
     print("Training:")
     common.getConfusion(df[learn, drug], predict(model, newdata=df[learn,]))
     print("Test:")
