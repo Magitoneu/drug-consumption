bd837b3d3375afe30e2c9e613c253afa0fd948f9
Magitoneu <mtoneupanicot@gmail.com>
2018-01-16 20:52:24 +0100

    crossval

--- common.R
+++ common.R
@@ -21,40 +21,47 @@ common.getSubstimateds = function(table) {
     subs = sum(df[select, 3])
 }
 
-
-#knnCAT, nnet,  RandomForest no necesiten la funció ja que la tenen incorporada (nnet té el metode train, RandomForest l'error OBS, knnCAT en la mateixa funció)
 common.crossval = function(k.folds, data, class.method){
-  shuffled.data <- data[sample(nrow(data)),]
-  folds <- cut(seq(1,nrow(shuffled.data)), breaks = k.folds, labels = FALSE)
-  error.cv <- 0
-  for(i in 1:k.folds){
-    testIndexes <- which(folds==i,arr.ind=TRUE)
-    print(nrow(shuffled.data[-testIndexes,]))
-    print(nrow(shuffled.data[testIndexes,]))
-    if(class.method == 'logisticregression'){
-      model <- glm(as.formula(paste(drug, " ~ .")), data=shuffled.data[-testIndexes,], family=binomial)
-      pred <- predict(model, newdata=shuffled.data[testIndexes,], type = "response")
-
-      gl1predt <- NULL
-      gl1predt[pred<0.5] <- 0
-      gl1predt[pred>=0.5] <- 1
-      pred <- factor(gl1predt, labels=c("CL0","CL1"), levels=c(0, 1))
+    shuffled.data <- data[sample(nrow(data)),]
+    folds <- cut(seq(1,nrow(shuffled.data)), breaks = k.folds, labels = FALSE)
+    error.cv <- 0
+    for(i in 1:k.folds){
+        testIndexes <- which(folds==i,arr.ind=TRUE)
+        if(class.method == 'logisticregression'){
+            model <- glm(as.formula(paste(drug, " ~ .")), data=shuffled.data[-testIndexes,], family=binomial)
+            pred <- predict(model, newdata=shuffled.data[testIndexes,], type = "response")
+            gl1predt <- NULL
+            gl1predt[pred<0.5] <- 0
+            gl1predt[pred>=0.5] <- 1
+            pred <- factor(gl1predt, labels=c("CL0","CL1"), levels=c(0, 1))
+        }
+        else if(class.method == 'naivebayes'){
+            model <- naiveBayes(as.formula(paste(drug, " ~ .")), data = shuffled.data[-testIndexes,])
+            pred <- predict(model, newdata=shuffled.data[testIndexes,])
+        }
+        else if(class.method == 'knn'){
+            model <- knn (shuffled.data[-testIndexes,1:12], shuffled.data[testIndexes,1:12], shuffled.data[-testIndexes,13], k = best.k, prob=TRUE)
+            pred <- model
+        }
+        else if(class.method == 'mlp'){
+            model <- nnet(as.formula(paste(drug, " ~ .")), data = shuffled.data[-testIndexes,], size = best.size, decay = best.decay, trace = F, maxit = 1000, MaxNWts = 1500)
+            pred <- predict(nnet.fit, newdata=shuffled.data[testIndexes,], type = 'class')
+        }
+        else {
+            print('Not implemented method.')
+        }
+        real = factor(shuffled.data[testIndexes, 13], levels=c("CL0", "CL1", "CL2", "CL3", "CL4", "CL5", "CL6"))
+        pred = factor(pred, levels=c("CL0", "CL1", "CL2", "CL3", "CL4", "CL5", "CL6"))
+        t  <- table(real, pred)
+        if(i == 1) table.cv = t
+        error.cv <- error.cv + (1-sum(diag(t))/length(shuffled.data[testIndexes, 13]))
+        table.cv <- table.cv + t
     }
-    else if(class.method == 'naivebayes'){
-      model <- naiveBayes(as.formula(paste(drug, " ~ .")), data = shuffled.data[-testIndexes,])
-      pred <- predict(model, newdata=shuffled.data[testIndexes,])
-    }
-    else {
-      print('Not implemented method.')
-    }
-    t  <- table(shuffled.data[testIndexes, 13], pred)
-    if(i == 1) table.cv = t
-    error.cv <- error.cv + (1-sum(diag(t))/length(shuffled.data[testIndexes, 13]))
-    table.cv <- table.cv + t
-  }
-  print(table.cv / k.folds)
-  print(paste('Cross-validation', class.method, 'Error:', (error.cv/k.folds)*100, sep = ' '))
-  ret = table.cv / k.folds
+    print((error.cv/k.folds)*100)
+    print(table.cv / k.folds)
+    #print(paste('Cross-validation', class.method, 'Error:', (error.cv/k.folds)*100, sep = ' '))
+    ret = table.cv / k.folds
+    return(ret)
 }
 
 
